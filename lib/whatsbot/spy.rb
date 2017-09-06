335af9e30f6aa6c7425207783faab6b5bb55f978
Marcelo Silva <marcelosilva.developer@gmail.com>
2017-09-06 00:34:10 -0300

    Refactor spy

--- lib/whatsbot/spy.rb
+++ lib/whatsbot/spy.rb
@@ -3,11 +3,14 @@ require 'selenium-webdriver'
 module Whatsbot
   class Spy
 
+    WHATSAPP_URL = 'https://web.whatsapp.com'
+    BROWSER_PROFILE = 'default'
+
     def initialize(config)
       raise 'A configuration must be passed' if config.nil?
 
-      driver = Selenium::WebDriver.for config.driver, profile: "default"
-      driver.navigate.to 'https://web.whatsapp.com'
+      driver = Selenium::WebDriver.for config.driver, profile: BROWSER_PROFILE
+      driver.navigate.to WHATSAPP_URL
     end
 
     private

5965c2a3d04469aec39c00c77e58d836a012520a
Marcelo Silva <marcelosilva.developer@gmail.com>
2017-09-10 20:49:42 -0300

    Add firefox profile

--- src/core/driver.js
+++ src/core/driver.js
@@ -1,9 +1,17 @@
 import webdriver from 'selenium-webdriver';
+import firefox from 'selenium-webdriver/firefox';
 
-const Driver = (browser) => {
-  return new webdriver.Builder()
-    .forBrowser(browser)
-    .build();
+const Driver = (browserName, browserProfile) => {
+  const browser = new webdriver.Builder()
+    .forBrowser(browserName);
+
+  switch (browserName) {
+    case 'firefox':
+      const profile = new firefox.Profile(browserProfile);
+      return browser.setFirefoxOptions(
+          new firefox.Options().setProfile(profile)
+      ).build();
+  }
 };
 
 export default Driver;
